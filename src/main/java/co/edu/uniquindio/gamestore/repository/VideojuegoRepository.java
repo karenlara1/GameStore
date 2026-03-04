f1276c8cf3c58a38dc181735a9728517e3727f5f
karenlara1 <karena.larab@uqvirtual.edu.co>
2026-03-03 21:28:08 -0500

    feat: Creación del repositorio VideojuegoRepository

---
+++ src/main/java/co/edu/uniquindio/gamestore/repository/VideojuegoRepository.java
@@ -0,0 +1,26 @@
+package co.edu.uniquindio.gamestore.repository;
+
+import co.edu.uniquindio.gamestore.entity.Genero;
+import co.edu.uniquindio.gamestore.entity.Videojuego;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface VideojuegoRepository extends JpaRepository<Videojuego, Long> {
+
+    //Spring construye la consulta automáticamente
+    List<Videojuego> findByGenero (Genero genero);
+
+    //Busca los titulos que contengan texto ignorando las mayusculas
+    List<Videojuego> findByTituloContainingIgnoreCase(String titulo);
+
+    //Consulta utilizando JPQL (orientado a las enidades)
+    @Query("SELECT v FROM Videojuego v WHERE v.precio BETWEEN :min AND :max")
+    List<Videojuego> buscarPorRangoPrecio (@Param("min") Double min, @Param("max") Double max);
+
+    //Consulta en SQL nativo
+    @Query(value = "SELECT * FROM videojuego ORDER BY fecha_creacion DESC LIMIT 5", nativeQuery = true)
+    List<Videojuego> ultimos5();
+}
